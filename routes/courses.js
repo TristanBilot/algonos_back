32c567f805b80a35a56c9c9b58eebb3feb82af2d
Tristan <bilot.tristan@hotmail.fr>
2019-12-17 22:57:16 +0100

    client-server

--- routes/courses.js
+++ routes/courses.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
     res.json([{data_structure: "Data structures"}, {algorithm: "Algorithm"}]);
 });
 
-router.get('/getCourse', (req, res) => {
+router.post('/', (req, res) => {
     const course = new Course({
         title: req.body.title
     });
@@ -17,6 +17,30 @@ router.get('/getCourse', (req, res) => {
     .catch(err => {
         res.json({message: err});
     });
+});
+
+router.get('/add', async (req, res) => {
+    console.log('add');
+    const course = new Course({
+        title: req.body.title
+    });
+    try {
+        const savedCourse = await course.save();
+        res.json(data);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+router.get('/find', async (req, res) => {
+    console.log('find');
+    try {
+        const course = await Course.find({}).then(function (courses) {
+            res.send(courses);
+        });
+    } catch (err) {
+        res.json({ message: err });
+    }
 });
 
 module.exports = router;
